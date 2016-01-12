7d6edced49c2a285f57c80fb955261333019ac25
panmari <smmuzi@gmail.com>
2016-01-12 20:22:41 +0100

    Fixed mode is_train=False in main.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import numpy as np
 import tensorflow as tf
-from time import gmtime, strftime
+import glob
 
 from model import DCGAN
 from utils import pp, save_images
+from input_pipeline_rendered_data import make_image_producer
 
 flags = tf.app.flags
 flags.DEFINE_integer("epoch", 25, "Epoch to train [25]")
@@ -30,20 +31,48 @@ def main(_):
 
     FLAGS.summary_dir = os.path.join(FLAGS.summary_dir, run_folder)
     FLAGS.checkpoint_dir = os.path.join(FLAGS.checkpoint_dir, run_folder)
+    if FLAGS.is_train:
+        gpu_options= tf.GPUOptions()
+    else:
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.01)
+    with tf.Session(config=tf.ConfigProto(log_device_placement=True, gpu_options=gpu_options)) as sess:
 
-    if not os.path.exists(FLAGS.checkpoint_dir):
-        os.makedirs(FLAGS.checkpoint_dir)
-    if not os.path.exists(FLAGS.summary_dir):
-        os.makedirs(FLAGS.summary_dir)
-
-    with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
-
-        dcgan = DCGAN(sess, image_size=FLAGS.image_size, batch_size=FLAGS.batch_size)
 
         if FLAGS.is_train:
+            dcgan = DCGAN(sess, image_size=FLAGS.image_size, batch_size=FLAGS.batch_size)
+            if not os.path.exists(FLAGS.checkpoint_dir):
+                os.makedirs(FLAGS.checkpoint_dir)
+            if not os.path.exists(FLAGS.summary_dir):
+                os.makedirs(FLAGS.summary_dir)
+
             dcgan.train(FLAGS)
         else:
-            dcgan.load(FLAGS.checkpoint_dir)
+            test_files = glob.glob('test_sketches/*64x64.png')
+            FLAGS.batch_size = 1    
+            with tf.device('/cpu:0'):
+                test_sketch_producer = make_image_producer(test_files, 1, 'test_sketches', 64,
+                                                          shuffle=False, whiten=False, color=False, capacity=1)
+                test_sketches = tf.train.batch([test_sketch_producer], batch_size=FLAGS.batch_size)
+
+                dcgan = DCGAN(sess, image_size=FLAGS.image_size, batch_size=FLAGS.batch_size, test_sketches=test_sketches)
+
+            used_checkpoint_dir = os.path.join(os.path.dirname(FLAGS.checkpoint_dir), '023') 
+            # Important: Since not all variables are restored, some need to be initialized here.
+            tf.initialize_all_variables().run()
+            dcgan.load(used_checkpoint_dir)
+            coord = tf.train.Coordinator()
+            tf.train.start_queue_runners(sess=sess, coord=coord)
+            try:
+                for filename in test_files:
+                    samples = sess.run(dcgan.G)
+                    save_images(samples, [1, 1], '{}_depth.png'.format(filename))
+            except tf.errors.OutOfRangeError as e:
+                print('Done training -- epoch limit reached')
+                raise e
+            finally:
+                # When done, ask the threads to stop.
+                coord.request_stop()
+
 
 if __name__ == '__main__':
     tf.app.run()
