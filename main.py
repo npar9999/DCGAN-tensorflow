611408fa2f93fe67e11e5a251fcd8924e405246e
panmari <smmuzi@gmail.com>
2016-01-19 16:50:32 +0100

    Some adaptions to make network easily run on other image sizes.

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ flags.DEFINE_integer("epoch", 25, "Epoch to train [25]")
 flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
 flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
 flags.DEFINE_integer("batch_size", 64, "The size of batch images [64]")
-flags.DEFINE_integer("image_size", 64, "The size of image to use (will be center cropped) [108]")
 flags.DEFINE_string("checkpoint_dir", "checkpoint_sketches_to_rendered", "Directory name to save the checkpoints [checkpoint]")
 flags.DEFINE_string("summary_dir", "summary_sketches_to_rendered", "Directory name to save the summaries [checkpoint]")
 flags.DEFINE_boolean("is_train", True, "True for training, False for testing [False]")
@@ -42,7 +41,7 @@ def main(_):
         gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.01)
     with tf.Session(config=tf.ConfigProto(log_device_placement=True, gpu_options=gpu_options)) as sess:
         if FLAGS.is_train:
-            dcgan = DCGAN(sess, image_size=FLAGS.image_size, batch_size=FLAGS.batch_size)
+            dcgan = DCGAN(sess, batch_size=FLAGS.batch_size)
             if not os.path.exists(FLAGS.checkpoint_dir):
                 os.makedirs(FLAGS.checkpoint_dir)
             if not os.path.exists(FLAGS.summary_dir):
@@ -57,7 +56,7 @@ def main(_):
                                                           shuffle=False, whiten=False, color=False, augment=False)
                 test_sketches = tf.train.batch([test_sketch_producer], batch_size=FLAGS.batch_size)
 
-                dcgan = DCGAN(sess, image_size=FLAGS.image_size, batch_size=FLAGS.batch_size, is_train=False)
+                dcgan = DCGAN(sess, batch_size=FLAGS.batch_size, is_train=False)
                 if dcgan.G.get_shape()[3] == 3:
                     sketches_for_display = tf.concat(3, [dcgan.sketches, dcgan.sketches, dcgan.sketches])
                 else:
@@ -65,7 +64,6 @@ def main(_):
                 # Put it together with sketch again for easy comparison
                 sample_with_sketch = tf.concat(0, [dcgan.G, sketches_for_display])
                 batch_z_shape = [FLAGS.batch_size, dcgan.z_dim]
-                rng = tf.random_uniform(batch_z_shape, minval=-1, maxval=1, dtype=tf.float32)
 
             run_restored = FLAGS.continue_from
             used_checkpoint_dir = os.path.join(os.path.dirname(FLAGS.checkpoint_dir), run_restored)
