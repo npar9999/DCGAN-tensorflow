d6c7dc10e0359b2f24a12dd79ee76ea9ef2b3277
panmari <smmuzi@gmail.com>
2016-01-25 13:56:28 +0100

    Removed unused code.

--- model.py
+++ model.py
@@ -191,17 +191,6 @@ class DCGAN(object):
         # Size after 4 convolutions with stride 2.
         downsampled_size = self.image_size // 2 ** 4
 
-        # s3_flat = tf.reshape(s3, [self.batch_size, self.gf_dim*8*downsampled_size*downsampled_size])
-        # self.abstract_representation = lrelu(self.g_s_bn4(linear(s3_flat, self.gfc_dim, 'g_s4_lin')))
-        # if z:
-        #     self.abstract_representation = tf.concat(1, [self.abstract_representation, z])
-        #
-        # # project `abstract representation` and reshape
-        # h0 = tf.reshape(linear(self.abstract_representation,
-        #                        self.gf_dim*8*downsampled_size*downsampled_size, 'g_h0_lin'),
-        #                 [-1, downsampled_size, downsampled_size, self.gf_dim * 8])
-        # h0 = tf.nn.relu(self.g_bn0(h0))
-
         z_slices = tf.mul(tf.ones([self.batch_size, downsampled_size, downsampled_size, self.z_dim]),
                           tf.reshape(self.z, [self.batch_size, 1, 1, self.z_dim]))
         self.abstract_representation = tf.concat(3, [s3, z_slices])
