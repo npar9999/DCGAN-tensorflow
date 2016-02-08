4b07d3bece1ce29e16404f81bede986c97737fc1
panmari <smmuzi@gmail.com>
2016-02-08 17:28:38 +0100

    Some adaptions to dumping to tensorbuffer to fail early on errors.

--- write_to_tensorbuffer.py
+++ write_to_tensorbuffer.py
@@ -8,8 +8,9 @@ from input_pipeline_rendered_data import *
 from random import shuffle
 
 tf.app.flags.DEFINE_string('directory', '/tmp', 'Place to dump file')
-tf.app.flags.DEFINE_string('name', 'recolor_chairs', 'Name of dump to be produced.')
-tf.app.flags.DEFINE_integer('examples_per_file', 2000, 'Number of examples per file.')
+tf.app.flags.DEFINE_string('name', 'all_chairs_sketch_rendered_64x64', 'Name of dump to be produced.')
+tf.app.flags.DEFINE_integer('examples_per_file', 2000, 'Number of examples per file. [2000]')
+tf.app.flags.DEFINE_integer('image_size', 64, 'Excpected width and length of all images, [64]')
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -33,9 +34,9 @@ def main(argv):
     #
     # rendered_files = get_rendered_files(chair_folder)
     # sketched_files = get_sketch_files(sketch_folder)
-
-    rendered_files = [x.strip() for x in open('recolor_experiment_shaded_images.txt').readlines()]
-    sketched_files = [x.strip() for x in open('recolor_experiment_sketch_images.txt').readlines()]
+    # TODO: Check if output files have all same size (except last one).
+    rendered_files = [x.strip() for x in open('../shapenet_chairs_rendered2/rendered_files.txt').readlines()]
+    sketched_files = [x.strip() for x in open('../shapenet_chairs_sketched2/sketch_files.txt').readlines()]
     if len(sketched_files) != len(rendered_files):
         raise Exception("Not same amount of files for different features.")
     print('Writing {} samples to {}'.format(len(rendered_files), filename))
@@ -44,6 +45,8 @@ def main(argv):
     for idx, (rendered_file, sketch_file) in list(enumerate(zipped)):
         pic = Image.open(rendered_file)
         pic_data = numpy.array(pic.getdata(), dtype=np.uint8)
+        if not pic_data.shape[0] == FLAGS.image_size * FLAGS.image_size:
+            raise Exception('Unexcpected image size for {}: {}'.format(rendered_file, pic_data.shape))
         if pic_data.shape[1] == 4:
             rendered = pic_data[:, 0:3]
         elif pic_data.shape[1] == 2:
@@ -54,12 +57,13 @@ def main(argv):
             raise Exception('Unexpected number of channels!')
 
         sketch = numpy.array(Image.open(sketch_file).getdata(), dtype=np.uint8)
+        if not sketch.shape[0] == FLAGS.image_size * FLAGS.image_size:
+            raise Exception('Unexcpected image size for {}: {}'.format(sketch_file, sketch.shape))
         if len(sketch.shape) == 2 and sketch.shape[1] == 2:
             sketch = sketch[:, 0]
         else:
             print('Skipped buggy sketch: ' + sketch_file)
             continue
-            #raise Exception('Unexpected number of channels: {}, file: {}'.format(sketch.shape, sketch_file))
 
         write_to(writer, sketch, rendered)
 
