3c3988911a9d8b73aa90d72dce390d4fb32c8fca
panmari <smmuzi@gmail.com>
2016-04-08 17:18:20 +0200

    Cluster Run 039: Reconstruct depth with 1 additional discriminator update.

--- model.py
+++ model.py
@@ -167,21 +167,15 @@ class DCGAN(object):
 
                 _, _, errD_fake, errD_real, errG = self.sess.run([d_optim, g_optim, self.d_loss_fake,
                                                                   self.d_loss_real, self.g_loss])
-                # Run g_optim more to make sure that d_loss does not go to zero (different from paper)
-                MAX_ADDITIONAL_UPDATES = 5
-                errD_fake_threshold = 1e-3
-                additional_G_runs = 0
-                while errD_fake < errD_fake_threshold and additional_G_runs < MAX_ADDITIONAL_UPDATES:
-                    [errD_fake, _] = self.sess.run([self.d_loss_fake, g_optim])
-                    additional_G_runs += 1
-
+                # Additional run for discriminator.
+                self.sess.run(d_optim)
                 toc = time.time()
 
                 counter += 1
                 duration = toc - tic
                 print("Run: %s, Step: [%4d] time: %5.1f, last iter: %1.2f (%1.4f e/s), d_loss: %.8f, g_loss: %.8f, G+: %2d"
                     % (run_string, counter, toc - start_time, duration, self.batch_size / duration,
-                       errD_fake+errD_real, errG, additional_G_runs))
+                       errD_fake+errD_real, errG, -1))
                 if counter % 50 == 0:
                     summary_str = self.sess.run(summary_op)
                     summary_writer.add_summary(summary_str, counter)
